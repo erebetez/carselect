5624ff91ed36ec7a8205c719dfdd8fea30ae9ad0
Etienne Rebetez <etienne.rebetez@gmail.com>
2018-05-22 18:16:08 +0200

    add express server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('"Hello World!"');
+});
+
+app.get('/save', function (req, res) {
+  res.send('"Saving the World"');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
